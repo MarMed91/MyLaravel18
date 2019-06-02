df844d65ff515f4dd32f4f12396e7ff22b451b08
MarkoM91 <markomedic91@gmail.com>
2019-06-02 20:18:48 +0200

    commit1

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@getLast5posts')->name('home');
+Route::get('category/{name}', 'CategoryController@getPostByCategoryName')->name('post.by.category');
+Route::get('admin/post/new', 'AdminController@createNewPost')->name('create.new.post');
+Route::post('admin/post/new', 'AdminController@saveNewPost')->name('save.new.post');
+Route::get('admin/post/edit/{id}', 'PostController@editPost')->name('edit.post');
+Route::patch('admin/post/edit/{id}', 'PostController@updatePost')->name('update.post');
